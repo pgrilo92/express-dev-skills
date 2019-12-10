c4917ce276739d990fd74367eb2c9c99c1b6dcd7
Joaquim <pgrilo92@hotmail.com>
2019-12-10 14:14:18 -0500

    add gitignore

--- generator/routes/skills.js
+++ generator/routes/skills.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('respond with a resource');
+});
+
+router.get('/show', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
